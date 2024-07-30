934586abf09c527cb042ef5f363e49244413c356
MatheusFariasx._ <145811351+matheuzindelas134@users.noreply.github.com>
2024-07-30 12:26:38 -0300

    media ponderada

---
+++ meda ponderada.js
@@ -0,0 +1,12 @@
+
+let n1 = 7.5;
+let n2 = 8.0;
+let n3 = 6.5;
+let p1 = 3;
+let p2 = 2;
+let p3 = 1;
+
+
+let media = (n1 * p1 + n2 * p2 + n3 * p3) / (p1 + p2 + p3);
+
+console.log("A média ponderada das notas é:", media);
